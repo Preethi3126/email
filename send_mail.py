110853d197e4bfd6a29acd72a5ad4391a66eb7b4
unknown <ravipreethi645@gmail.com>
2023-06-08 13:29:00 +0530

    email

---
+++ send_mail.py
@@ -0,0 +1,27 @@
+import smtplib
+from email.mime.text import MIMEText
+
+def send_email():
+    sender_email = "ravipreethi645@gmail.com"
+    receiver_email = "johnvijay7345@gmail.com"
+    subject = "Test Email"
+    message = "Hi this is Preethi. You Have a Scrum call @10:30 A.M ."
+
+    msg = MIMEText(message)
+    msg["Subject"] = subject
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+
+    try:
+        smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
+        smtp_server.starttls()
+        smtp_server.login(sender_email, "Preethi$8541")
+        smtp_server.sendmail(sender_email, receiver_email, msg.as_string())
+        smtp_server.quit()
+        print("Email sent successfully!")
+    except smtplib.SMTPException as e:
+        print("Error: unable to send email.")
+        print(e)
+
+if __name__ == "__main__":
+    send_email()
