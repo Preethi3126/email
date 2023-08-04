1e687ab412c9d9db054c2d119437f16371c86de2
unknown <ravipreethi645@gmail.com>
2023-08-04 10:52:02 +0530

    password changed

--- send_mail.py
+++ send_mail.py
@@ -3,7 +3,7 @@ from email.mime.text import MIMEText
 #this is a sample text
 def send_email():
     sender_email = "ravipreethi645@gmail.com"
-    receiver_email = "vijayjohn7345@gmail.com"
+    receiver_email = "preethi.ravi@axesstechnology.in"
     subject = "Test Email"
     message = "Hi this is Preethi. You Have a Scrum call @10:30 A.M ."
 
@@ -15,7 +15,7 @@ def send_email():
     try:
         smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
         smtp_server.starttls()
-        smtp_server.login(sender_email, "vejm hvle fgun ioxt")
+        smtp_server.login(sender_email, "pemi tdzc lobk wfql")
         smtp_server.sendmail(sender_email, receiver_email, msg.as_string())
         smtp_server.quit()
         print("Email sent successfully!")
